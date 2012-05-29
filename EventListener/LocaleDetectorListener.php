72493dc792c3e4a1af312f1d3fc45f6a2f043901
Guilherme Blanco <guilhermeblanco@gmail.com>
2012-05-29 15:51:35 -0300

    Update EventListener/LocaleDetectorListener.php

--- EventListener/LocaleDetectorListener.php
+++ EventListener/LocaleDetectorListener.php
@@ -2,48 +2,42 @@
 
 namespace Lunetics\LocaleBundle\EventListener;
 
-use Symfony\Component\EventDispatcher\Event;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\HttpKernel\Log\LoggerInterface;
-use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
 use Symfony\Component\HttpFoundation\Cookie;
+
+use Symfony\Component\HttpKernel\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
  * Class for the Locale Detector
  *
  * Detects and sets the Locale
  */
-class LocaleDetectorListener
+class LocaleDetectorListener implements EventSubscriberInterface
 {
     /**
      * @var array
      */
     private $availableLanguages = array();
+
     /**
      * @var null|\Symfony\Component\HttpKernel\Log\LoggerInterface
      */
     private $logger;
+
     /**
      * @var string
      */
     private $defaultLocale;
 
     /**
-     * @var \Symfony\Component\EventDispatcher\EventDispatcher
-     */
-    private $dispatcher;
-
-    /**
-     * @var boolean
-     */
-    private $cookieListenerisAdded = false;
-
-    /**
      * Setup the Locale Listener
      *
-     * @param                                                        $defaultLocale      The default Locale
-     * @param                                                        $availableLanguages List of available / allowed locales
+     * @param string                                                 $defaultLocale      The default Locale
+     * @param array                                                  $availableLanguages List of available / allowed locales
      * @param null|\Symfony\Component\HttpKernel\Log\LoggerInterface $logger             Logger Interface
      */
     public function __construct($defaultLocale, $availableLanguages, LoggerInterface $logger = null)
@@ -52,63 +46,34 @@ class LocaleDetectorListener
         $this->logger = $logger;
         $this->availableLanguages = $availableLanguages;
     }
+
     /**
-     * DI Setter for the EventDispatcher
+     * The Request Listener which sets the locale
      *
-     * @param \Symfony\Component\EventDispatcher\EventDispatcher $dispatcher
+     * @param Symfony\Component\HttpKernel\Event\GetResponseEvent $event
      *
      * @return void
      */
-    public function setEventDispatcher(EventDispatcher $dispatcher)
+    public function onKernelRequest(GetResponseEvent $event)
     {
-        $this->dispatcher = $dispatcher;
-    }
-
-    /**
-     * Method to add the ResponseListener which sets the cookie. Should only be called once
-     *
-     * @see also http://slides.seld.be/?file=2011-10-20+High+Performance+Websites+with+Symfony2.html#45
-     *
-     * @return void
-     */
-    public function addCookieResponseListener()
-    {
-        if($this->cookieListenerisAdded !== true) {
-            $this->dispatcher->addListener(
-                KernelEvents::RESPONSE,
-                array($this, 'onResponse')
-            );
-        }
-        $this->cookieListenerisAdded = true;
-    }
-
-    /**
-     *  The Request Listener which sets the locale
-     *
-     * @param \Symfony\Component\EventDispatcher\Event $event
-     *
-     * @return void
-     */
-    public function onRequest(Event $event)
-    {
-
         $request = $event->getRequest();
         /* @var $request \Symfony\Component\HttpFoundation\Request */
 
         $session = $request->getSession();
         /* @var $session \Symfony\Component\HttpFoundation\Session */
 
-       if($session->has('setLocaleCookie') || !$request->cookies->has('locale')) {
-          $session->remove('setLocaleCookie');
-          $this->addCookieResponseListener();
-       }
+        if ($session->has('setLocaleCookie') || !$request->cookies->has('locale')) {
+            $session->remove('setLocaleCookie');
+        }
 
         // Check if the locale has been identified, no repeating locale checks on subsequent requests needed
         if ($session->has('localeIdentified')) {
             $request->setLocale($session->get('localeIdentified'));
+
             if (null !== $this->logger) {
                 $this->logger->info(sprintf('Locale already Identified : [ %s ]', $session->get('localeIdentified')));
             }
+
             return;
         }
 
@@ -119,15 +84,17 @@ class LocaleDetectorListener
         if (!$preferredLanguage OR count($providedLanguages) === 0) {
             $preferredLanguage = $this->defaultLocale;
         } else if (!in_array(\Locale::getPrimaryLanguage($preferredLanguage), $this->availableLanguages)) {
-
             $availableLanguages = $this->availableLanguages;
+
             $map = function($v) use ($availableLanguages)
             {
                 if (in_array(\Locale::getPrimaryLanguage($v), $availableLanguages)) {
                     return true;
                 }
             };
+
             $result = array_values(array_filter($providedLanguages, $map));
+
             if (!empty($result)) {
                 $preferredLanguage = $result[0];
             } else {
@@ -137,14 +104,20 @@ class LocaleDetectorListener
 
         $request->setLocale($preferredLanguage);
         $session->set('localeIdentified', $preferredLanguage);
+
         if (null !== $this->logger) {
             $this->logger->info(sprintf('Locale detected: [ %s ]', $request->getLocale()));
         }
-        $this->addCookieResponseListener();
+
         return;
     }
 
-    public function onResponse(Event  $event)
+    /**
+     * The Response Listener which persists the locale
+     *
+     * @param Symfony\Component\HttpKernel\Event\FilterResponseEvent $event
+     */
+    public function onKernelResponse(FilterResponseEvent $event)
     {
         $response = $event->getResponse();
         /* @var $response \Symfony\Component\HttpFoundation\Response */
@@ -153,8 +126,18 @@ class LocaleDetectorListener
         /* @var $session \Symfony\Component\HttpFoundation\Session */
 
         $response->headers->setCookie(new Cookie('locale', $session->get('localeIdentified')));
+
         if (null !== $this->logger) {
             $this->logger->info(sprintf('Locale Cookie set to: [ %s ]', $session->get('localeIdentified')));
         }
+    }
+
+    static public function getSubscribedEvents()
+    {
+        return array(
+            // must be registered after the Router to have access to the _locale
+            KernelEvents::REQUEST  => array(array('onKernelRequest', 8)),
+            KernelEvents::RESPONSE => array(array('onKernelResponse', 8))
+        );
     }
 }
